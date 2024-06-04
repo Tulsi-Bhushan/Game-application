e82b326a99dcee43e3fd3906c8b5e5d0eedf6a2b
Tulsi-Bhushan <117098319+Tulsi-Bhushan@users.noreply.github.com>
2024-06-04 19:49:48 +0530

    Create candyData.ts

---
+++ candy-crush-clone/src/utils/candyData.ts
@@ -0,0 +1,17 @@
+import candyOne from "../assests/candyOne.png";
+import candyTwo from "../assests/candyTwo.png";
+import candyThree from "../assests/candyThree.png";
+import candyFour from "../assests/candyFour.png";
+import candyFive from "../assests/candyFive.png";
+import candySix from "../assests/candySix.png";
+import candySeven from "../assests/candySeven.png";
+
+export const candies = [
+  candyOne,
+  candyTwo,
+  candyThree,
+  candyFour,
+  candyFive,
+  candySix,
+  candySeven,
+];
